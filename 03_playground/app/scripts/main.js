c1e71627cb0f3b5e8008832c5a1bf64ed7deb232
Nicolas Roos <nicolas.k.roos@gmail.com>
2016-06-03 16:36:28 +0200

    added node module request

--- 03_playground/app/scripts/main.js
+++ 03_playground/app/scripts/main.js
@@ -1,48 +1,32 @@
-import * as CONFIG from './config';
-import jsSHA from 'jssha';
 
 
 let postArray  = {};
 postArray['action'] = 'getProductList';
-postArray['clientIp'] = '127.0.0.1';
+//postArray['clientIp'] = '127.0.0.1';
 postArray['responseFormat'] = 'json';
 
 
 
-let shaObj = new jsSHA('SHA-256', 'TEXT');
-shaObj.setHMACKey(CONFIG.LOBO_API_PRIVATE_KEY, 'TEXT');
-shaObj.update(JSON.stringify(postArray));
-let publicHash = shaObj.getHMAC("HEX");
-
 var myHeaders = new Headers();
 myHeaders.set("Content-Type", "application/json");
-myHeaders.set('Accept','application/json, application/xml, text/plain, text/html, *.*');
-myHeaders.set('X-Client-Id',CONFIG.LOBO_API_CLIENT_ID);
-myHeaders.set('X-Public-Hash',publicHash);
+myHeaders.set('Accept','application/json, text/plain, text/html');
+//myHeaders.set('X-Client-Id',CONFIG.LOBO_API_CLIENT_ID);
+//myHeaders.set('X-Public-Hash',publicHash);
 
-let formData = new URLSearchParams();
+/*let formData = new URLSearchParams();
 for(let key in postArray) {
     console.log(key,postArray[key]);
     formData.append(key,postArray[key]);
-}
+}*/
 
 var myInit = {
-    method: 'POST',
-    headers: myHeaders,
-    mode: 'no-cors',
-    body: formData
+    method: 'GET',
+    //headers: myHeaders,
+    body: postArray
 };
 
-
-
-console.log(myInit);
-
-console.log(myHeaders.getAll('X-Public-Hash'));
-console.log(myHeaders.getAll('X-Client-Id'));
-
-fetch(CONFIG.LOBO_API_URL,myInit).then(function (data) {
-    //console.log(data.json());
-    //return data.json();
+fetch('/api',myInit).then(function (data) {
+    return data.json();
 }).then(function (json) {
     console.log(json);
 });
--- 03_playground/server/server.js
+++ 03_playground/server/server.js
@@ -1,10 +1,16 @@
-var express = require('express');
+import express from 'express';
+import request from 'request';
+import * as CONFIG from './config';
 var app = express();
 
 
 
 app.get('/api', function (req, res) {
+    //request.post(CONFIG.LOBO_API_URL)
+
     console.log('Executing API call');
+    res.json({test: 'test'});
+    //res.sendStatus(200);
 });
 
 app.use('/', express.static(__dirname + '/app'));
