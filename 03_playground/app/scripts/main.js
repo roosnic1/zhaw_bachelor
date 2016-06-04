eaf4aa4affd20e43948a1789e5773d067c6377f1
Nicolas Roos <nicolas.k.roos@gmail.com>
2016-06-04 12:51:18 +0200

    send request from client to server and then to lobo

--- 03_playground/app/scripts/main.js
+++ 03_playground/app/scripts/main.js
@@ -2,27 +2,19 @@
 
 let postArray  = {};
 postArray['action'] = 'getProductList';
-//postArray['clientIp'] = '127.0.0.1';
+postArray['clientIp'] = '127.0.0.1';
 postArray['responseFormat'] = 'json';
 
 
 
-var myHeaders = new Headers();
-myHeaders.set("Content-Type", "application/json");
-myHeaders.set('Accept','application/json, text/plain, text/html');
-//myHeaders.set('X-Client-Id',CONFIG.LOBO_API_CLIENT_ID);
-//myHeaders.set('X-Public-Hash',publicHash);
-
-/*let formData = new URLSearchParams();
-for(let key in postArray) {
-    console.log(key,postArray[key]);
-    formData.append(key,postArray[key]);
-}*/
 
 var myInit = {
-    method: 'GET',
-    //headers: myHeaders,
-    body: postArray
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(postArray)
 };
 
 fetch('/api',myInit).then(function (data) {
--- 03_playground/server/server.js
+++ 03_playground/server/server.js
@@ -1,16 +1,45 @@
 import express from 'express';
 import request from 'request';
+import jsSHA from 'jssha';
+import bodyParser from 'body-parser';
 import * as CONFIG from './config';
 var app = express();
 
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api', function (req, res) {
-    //request.post(CONFIG.LOBO_API_URL)
+app.post('/api', function (req, res) {
+    if(!req.body) {
+        res.json({error: 'No Data in request'});
+    }
 
-    console.log('Executing API call');
-    res.json({test: 'test'});
-    //res.sendStatus(200);
+    let postArray = req.body;
+
+    let shaObj = new jsSHA('SHA-256', "TEXT");
+    shaObj.setHMACKey(CONFIG.LOBO_API_PRIVATE_KEY, "TEXT");
+    shaObj.update(JSON.stringify(postArray));
+
+
+    let opt = {
+        url: CONFIG.LOBO_API_URL,
+        method: 'POST',
+        headers: {
+            'X-Client-Id': CONFIG.LOBO_API_CLIENT_ID,
+            'X-Public-Hash': shaObj.getHMAC("HEX"),
+        },
+        form: postArray
+    };
+    request.post(opt, function (err,response,body) {
+        if(err) {
+            console.log('ERROR: ',err);
+            res.json({error:err});
+            return;
+        }
+
+        console.log(body);
+        res.json(body);
+    });
 });
 
 app.use('/', express.static(__dirname + '/app'));
