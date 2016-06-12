7e5d2aabbc3483e2b2a6cea31e45da5a42728b3a
Nicolas Roos <nicolas.k.roos@gmail.com>
2016-06-12 22:09:21 +0200

    added another api function

--- 04_poc/server/api/index.js
+++ 04_poc/server/api/index.js
@@ -6,13 +6,12 @@ const CONFIG = require('./config');
 // create new API router;
 const apiRouter = express.Router();
 
-function createLoboRequest(action,parametrs) {
-    "use strict";
-    const postArray  = {
+function createLoboRequest(action,params = {}) {
+    const postArray  = Object.assign({
         action: action,
         clientIp: '127.0.0.1',
         responseFormat: 'json'
-    };
+    },params);
 
     const shaObj = new JsSHA('SHA-256', "TEXT");
     shaObj.setHMACKey(CONFIG.LOBO_API_PRIVATE_KEY, "TEXT");
@@ -56,6 +55,24 @@ apiRouter.connectToLobo = function () {
 
 apiRouter.get('/productlist', function (req,res, next) {
     const opt = createLoboRequest('getProductList');
+    request.post(opt, function (err,response,body) {
+        if(err) {
+            res.json({error:err});
+        } else {
+            res.send(body);
+        }
+    });
+});
+
+apiRouter.get('/verifyAdress',function (req,res, next) {
+    const params = {
+        isocode: 'CHE',
+        zip: '8004',
+        street: 'Stauffacherstrasse',
+        housenumber: 223
+    };
+    const opt = createLoboRequest('verifyAddress',params);
+    console.log(opt);
     request.post(opt, function (err,response,body) {
         if(err) {
             res.json({error:err});
